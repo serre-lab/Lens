ebd4054533c775d71b58ae8565d476d66127d885
Thomas Fel <thomas_fel@brown.edu>
2024-02-20 20:54:30 +0100

    Deployed e9e96a0 with MkDocs version: 1.5.3

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,3 @@
-MathJax.Hub.Config({
-    config: ["MMLorHTML.js"],
-    jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
-    extensions: ["MathMenu.js", "MathZoom.js"]
-});
-
-MathJax.Hub.Config({
-    config: ["MMLorHTML.js"],
-    extensions: ["tex2jax.js"],
-    jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
-    tex2jax: {
-        inlineMath: [["\\(", "\\)"]],
-        displayMath: [["\\[", "\\]"]],
-        processEscapes: true,
-        processEnvironments: true,
-        ignoreClass: ".*|",
-        processClass: "arithmatex"
-    },
-});
-
 const translate_size = 140
 const carts = [...document.querySelectorAll('.feature-viz-intro .gallery-container-img')]
 
